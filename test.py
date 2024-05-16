f42fc56628ef4a88679ac8b59b00fcc569c5f786
Dmitry Mordanov <mordanov314@gmail.com>
2024-05-16 10:21:50 +0300

    near to final

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from audio_recognition import AudioRecognition
 
 ai = AudioRecognition()
+
+#file = 'C:\\Work\\python\\Vodokanal\\autio_recognition\\upload\\zayka.mp3'
 file = '/opt/audio_recognition/upload/zayka.mp3'
+
 result = ai.recognition(file)
+
 print(result)
